144456e6a324dfc8c86c76b2f089d9779b585ba9
juanda <jdrincone@gmail.com>
2025-06-18 14:28:57 -0500

    feat: actualiza postgres_loader.py

--- etl/postgres_loader.py
+++ etl/postgres_loader.py
@@ -1,6 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
+import logging
 
 def load_to_postgres(df: pd.DataFrame, db_uri: str, table_name: str):
     engine = create_engine(db_uri)
-    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    with engine.begin() as conn:
+        logging.info(f"📤 Cargando datos en tabla {table_name}")
+        df.to_sql(table_name, conn, if_exists='replace', index=False)
+
+        logging.info("🔧 Ejecutando permisos y propietario...")
+        conn.execute(text(f"ALTER TABLE public.{table_name} OWNER TO postgres;"))
+        conn.execute(text(f"""
+            GRANT DELETE, INSERT, REFERENCES, SELECT, TRIGGER, TRUNCATE, UPDATE
+            ON public.{table_name} TO n8n;
+        """))
+
+    logging.info("✅ Carga completada y permisos asignados")
